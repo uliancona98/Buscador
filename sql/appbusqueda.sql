544550dd3c86e7db83eb8cf4f06f7f1b4c4341ca
cavesdev <24899540+cavesdev@users.noreply.github.com>
2021-02-02 17:46:22 -0800

    reorganized files

--- sql/appbusqueda.sql
+++ sql/appbusqueda.sql
@@ -18,13 +18,13 @@ USE `appbusqueda`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `autor`
+-- Table structure for table `autores`
 --
 
-DROP TABLE IF EXISTS `autor`;
+DROP TABLE IF EXISTS `autores`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `autor` (
+CREATE TABLE `autores` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
   `last_modified` datetime NOT NULL,
@@ -33,23 +33,23 @@ CREATE TABLE `autor` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `autor`
+-- Dumping data for table `autores`
 --
 
-LOCK TABLES `autor` WRITE;
-/*!40000 ALTER TABLE `autor` DISABLE KEYS */;
-INSERT INTO `autor` VALUES (1,'Miguel',NOW()),(5,'Pablo',NOW()),(6,'Francisco',NOW());
-/*!40000 ALTER TABLE `autor` ENABLE KEYS */;
+LOCK TABLES `autores` WRITE;
+/*!40000 ALTER TABLE `autores` DISABLE KEYS */;
+INSERT INTO `autores` VALUES (1,'Miguel',NOW()),(5,'Pablo',NOW()),(6,'Francisco',NOW());
+/*!40000 ALTER TABLE `autores` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `autor_libro`
 --
 
-DROP TABLE IF EXISTS `autor_libro`;
+DROP TABLE IF EXISTS `autores_libros`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `autor_libro` (
+CREATE TABLE `autores_libros` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_libro` int NOT NULL,
   `id_autor` int NOT NULL,
@@ -59,23 +59,23 @@ CREATE TABLE `autor_libro` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `autor_libro`
+-- Dumping data for table `autores_libros`
 --
 
-LOCK TABLES `autor_libro` WRITE;
-/*!40000 ALTER TABLE `autor_libro` DISABLE KEYS */;
-INSERT INTO `autor_libro` VALUES (1,2,1,NOW()),(2,3,1,NOW()),(3,3,5,NOW());
-/*!40000 ALTER TABLE `autor_libro` ENABLE KEYS */;
+LOCK TABLES `autores_libros` WRITE;
+/*!40000 ALTER TABLE `autores_libros` DISABLE KEYS */;
+INSERT INTO `autores_libros` VALUES (1,2,1,NOW()),(2,3,1,NOW()),(3,3,5,NOW());
+/*!40000 ALTER TABLE `autores_libros` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `libro`
+-- Table structure for table `libros`
 --
 
-DROP TABLE IF EXISTS `libro`;
+DROP TABLE IF EXISTS `libros`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `libro` (
+CREATE TABLE `libros` (
   `id` int NOT NULL AUTO_INCREMENT,
   `titulo` varchar(45) NOT NULL,
   `editorial` varchar(45) NOT NULL,
@@ -85,23 +85,23 @@ CREATE TABLE `libro` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `libro`
+-- Dumping data for table `libros`
 --
 
-LOCK TABLES `libro` WRITE;
-/*!40000 ALTER TABLE `libro` DISABLE KEYS */;
-INSERT INTO `libro` VALUES (1,'La Odisea','Greek',NOW()),(2,'La Iliada','Greek',NOW()),(3,'La ALGO','Chalupa',NOW()),(4,'Simpson','Panini',NOW()),(5,'Libro nuevo','laquesea',NOW());
-/*!40000 ALTER TABLE `libro` ENABLE KEYS */;
+LOCK TABLES `libros` WRITE;
+/*!40000 ALTER TABLE `libros` DISABLE KEYS */;
+INSERT INTO `libros` VALUES (1,'La Odisea','Greek',NOW()),(2,'La Iliada','Greek',NOW()),(3,'La ALGO','Chalupa',NOW()),(4,'Simpson','Panini',NOW()),(5,'Libro nuevo','laquesea',NOW());
+/*!40000 ALTER TABLE `libros` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `usuario`
+-- Table structure for table `usuarios`
 --
 
-DROP TABLE IF EXISTS `usuario`;
+DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuario` (
+CREATE TABLE `usuarios` (
   `id` int NOT NULL AUTO_INCREMENT,
   `usuario` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
@@ -112,13 +112,13 @@ CREATE TABLE `usuario` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `usuario`
+-- Dumping data for table `usuarios`
 --
 
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'carlos','12345','d7d6cd8f-0ba0-4324-88eb-6bb0f43cb2a4'),(2,'ulises','12345','150a04ae-a4d6-483c-aa0e-277b832d49d6'),(4,'shaid','12345','150a04ae-a4d6-483c-aa0e-852b832d49d6');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'carlos','12345','d7d6cd8f-0ba0-4324-88eb-6bb0f43cb2a4'),(2,'ulises','12345','150a04ae-a4d6-483c-aa0e-277b832d49d6'),(4,'shaid','12345','150a04ae-a4d6-483c-aa0e-852b832d49d6');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
