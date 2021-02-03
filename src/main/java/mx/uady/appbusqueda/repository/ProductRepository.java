a91dfc6493dee750c48b1fed870d8247449f42e2
Ulises Ancona <uliancona@hotmail.com>
2021-02-02 20:19:31 -0600

    CAMBIOS

--- src/main/java/mx/uady/appbusqueda/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package mx.uady.appbusqueda.repository;
-
-
-import mx.uady.appbusqueda.model.*;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ProductRepository extends PagingAndSortingRepository<ProductEntity,Integer> {
-}
